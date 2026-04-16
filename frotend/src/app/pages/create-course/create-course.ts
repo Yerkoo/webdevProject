1fee36a8af7052ad77e64c8ec340781ca7e1edc8
Yerkoo <123399269+Yerkoo@users.noreply.github.com>
2026-04-16 12:31:24 +0500

    createcourse, html css

--- frotend/src/app/pages/create-course/create-course.ts
+++ frotend/src/app/pages/create-course/create-course.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-course',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule], // Оставили только формы
   templateUrl: './create-course.html',
-  styleUrl: './create-course.css',
+  styleUrl: './create-course.css'
 })
-export class CreateCourse {}
+export class CreateCourse {
+  courseName: string = '';
+  courseDescription: string = '';
+  youtubeLink: string = '';
+
+  onSubmit() {
+    console.log('Готовим к отправке на бэкенд:', {
+      title: this.courseName,
+      description: this.courseDescription,
+      video_url: this.youtubeLink
+    });
+  }
+}
