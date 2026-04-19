2200d093907519882da38f0d80c9fa716fa67186
Yerkoo <123399269+Yerkoo@users.noreply.github.com>
2026-04-19 19:15:04 +0500

    category

--- frotend/src/app/pages/create-course/create-course.ts
+++ frotend/src/app/pages/create-course/create-course.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-course',
@@ -9,18 +10,45 @@ import { CommonModule } from '@angular/common';
   templateUrl: './create-course.html',
   styleUrl: './create-course.css'
 })
-export class CreateCourse {
+export class CreateCourse implements OnInit {
   courseName: string = '';
   courseDescription: string = '';
   youtubeLink: string = '';
   avatarUrl: string = '';
 
-  onSubmit() {
-    console.log('Готовим к отправке на бэкенд:', {
+  categories: any[] = []; 
+  selectedCategoryId: string = ''; 
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchCategories();
+  }
+
+  fetchCategories() {
+    this.http.get<any[]>('http://127.0.0.1:8000/api/categories/').subscribe({
+      next: (data) => {
+        this.categories = data;
+        console.log('Successfully loaded categories:', this.categories);
+      },
+      error: (err) => {
+        console.error('Error loading categories (is backend running?)', err);
+      }
+    });
+  }
+
+
+
+ onSubmit() {
+    const newCourse = {
       title: this.courseName,
       description: this.courseDescription,
       video_url: this.youtubeLink,
-      avatar_url: this.avatarUrl
-    });
+      avatar_url: this.avatarUrl,
+      category: this.selectedCategoryId 
+    };
+
+    console.log('Отправляем на бэкенд:', newCourse);
+    // Чуть позже тут будет this.http.post(...)
   }
 }
